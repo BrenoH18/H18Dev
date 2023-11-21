b4b018f4547f09eb32bcb50323244357d901f147
Breno Henrique <bhgames13@gmail.com>
2023-11-21 17:37:16 -0300

    ...

--- includes/header-index.php
+++ includes/header-index.php
@@ -21,7 +21,6 @@
                 echo "<a href='templates/vendas.php?id=$id' class='icon 'id='icon-vendas'>Venda de Produtos</a>";
             echo"</nav>";
         }elseif($user_data['permissao'] == 'user'){
-        }else{
             header("Location: ../index.php?id=$user_data[id]");
         }
     };
--- index.php
+++ index.php
@@ -10,7 +10,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>H18 Dev | Home</title>
         <link rel="stylesheet" href="static/css/reset.css">
-        <link rel="stylesheet" href="static/css/footer.css">
         <link rel="stylesheet" href="static/css/index.css">
     </head>
     <body>
