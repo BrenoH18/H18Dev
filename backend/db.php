06b2c16accbfcfb897d2ed44c8fc8715608573d8
Breno Henrique <bhgames13@gmail.com>
2024-08-19 22:07:15 -0300

    update code

--- backend/db.php
+++ backend/db.php
@@ -22,12 +22,12 @@
     // SQL para criar a tabela usuarios
     $sqlCreateTableUsuario = "CREATE TABLE IF NOT EXISTS usuarios (
         id INT AUTO_INCREMENT PRIMARY KEY,
-        nome VARCHAR(45),
-        sobrenome VARCHAR(45),
-        email VARCHAR(45),
-        senha VARCHAR(255),
-        empresa VARCHAR(45),
-        permissao VARCHAR(45)
+        nome VARCHAR(45) NOT NULL,
+        sobrenome VARCHAR(45) NOT NULL,
+        email VARCHAR(45) NOT NULL,
+        senha VARCHAR(255) NOT NULL,
+        empresa VARCHAR(45) NOT NULL,
+        permissao VARCHAR(45) NOT NULL DEFAULT 'user'
     )";
 
     if ($db->query($sqlCreateTableUsuario) === FALSE) {
