87396ce10c70fa165f68ff8908301bbf8d926368
Breno Henrique <77417897+BrenoH18@users.noreply.github.com>
2024-08-21 07:03:58 -0300

    Atualizar o alert.js

--- static/js/alert.js
+++ static/js/alert.js
@@ -1,13 +1,13 @@
 // Obter a URL atual
 const url = new URL(window.location.href);
 
-// Criar um objeto URLSearchParams a partir dos parâmetros da URL
+// Criar constante params a partir dos parâmetros da URL
 const params = new URLSearchParams(url.search);
 
 // Obter o valor do parâmetro "alert"
 const alertValue = params.get('alert');
 
-// Exibir o valor
+// Exibir o valor se for diferente de null
 if(alertValue !== null){
     alert(alertValue);
 }
