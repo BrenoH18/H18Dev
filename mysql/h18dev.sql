d08bfafac80aef42066c0861250a5dd08647a4f6
Breno Henrique <bhgames13@gmail.com>
2024-08-20 21:02:44 -0300

    delete sql

--- mysql/h18dev.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS usuarios (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        nome VARCHAR(45) NOT NULL,
-        sobrenome VARCHAR(45) NOT NULL,
-        email VARCHAR(45) NOT NULL,
-        senha VARCHAR(255) NOT NULL,
-        empresa VARCHAR(45) NOT NULL,
-        permissao VARCHAR(45) NOT NULL DEFAULT 'user'
-    )
