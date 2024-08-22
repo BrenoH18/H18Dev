af4fb67d221792eab871fbd6fc07b9e4c5b5305e
Breno Henrique <77417897+BrenoH18@users.noreply.github.com>
2024-08-22 00:15:32 -0300

    Atualizar o autenticacao.php

--- backend/autenticacao.php
+++ backend/autenticacao.php
@@ -1,34 +1,38 @@
 <?php
-    session_start();
+    session_start(); // Inicia a sessão
 
-    if(isset($_POST['submit'])){
-        include_once('db.php');
+    if(isset($_POST['submit'])){ // Verifica se o formulário foi submetido
+        include_once('db.php'); // Inclui o arquivo de conexão com o banco de dados
 
-        $email = $_POST['email'];
-        $senha = $_POST['senha'];
+        $email = $_POST['email']; // Obtém o email do formulário
+        $senha = $_POST['senha']; // Obtém a senha do formulário
 
-
+        // Consulta o banco de dados para encontrar o usuário pelo email
         $sql = "SELECT * FROM usuarios WHERE email = '$email' LIMIT 1";
         $result = $db->query($sql) or die($db->error);
-        $user_data = mysqli_fetch_assoc($result);
+        $user_data = mysqli_fetch_assoc($result); // Recupera os dados do usuário como um array associativo
 
-        if($user_data){
+        if($user_data){ // Verifica se o usuário foi encontrado no banco de dados
     
-            if(password_verify($senha, $user_data['senha'])){
-                $_SESSION['logado'] = true;
-                $_SESSION['email'] = $email;
+            if(password_verify($senha, $user_data['senha'])){ // Verifica se a senha informada corresponde à senha no banco de dados
+                $_SESSION['logado'] = true; // Define a sessão como logada
+                $_SESSION['email'] = $email; // Armazena o email do usuário na sessão
                 
+                // Redireciona para a página principal com uma mensagem de sucesso
                 header("Location: ../templates/index.php?id=$user_data[id]?alert=login_alert&mensagem=Login efetuado com sucesso!");
             }else{
+                // Se a senha estiver incorreta, define a sessão como não logada e redireciona para a página de login com uma mensagem de erro
                 $_SESSION['logado'] = false;
                 header('Location: ../templates/login.html?alert=E-mail ou senha incorreto, tente novamente!');
             }
         }else{
+            // Se o usuário não for encontrado no banco de dados, define a sessão como não logada e redireciona para a página de login com uma mensagem de erro
             $_SESSION['logado'] = false;
-                header('Location: ../templates/login.html?alert=E-mail ou senha incorreto, tente novamente!');
+            header('Location: ../templates/login.html?alert=E-mail ou senha incorreto, tente novamente!');
         }
         
     }else{
+        // Se o formulário não foi submetido, redireciona para a página de login
         header('Location: ../templates/login.html');
     }
 ?>
